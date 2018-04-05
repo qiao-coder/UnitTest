12d510e9accfe8c7b9e133b1b2147227c4391178
tufei <1024522245@qq.com>
2018-04-05 23:57:08 +0800

    mockito测试

--- app/src/main/java/com/tufei/unittest/mockito/MockitoSample.java
+++ app/src/main/java/com/tufei/unittest/mockito/MockitoSample.java
@@ -1,5 +1,10 @@
 package com.tufei.unittest.mockito;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author TuFei
  * @date 2018/4/2.
@@ -9,12 +14,36 @@ public class MockitoSample {
     public int publicInt = 1;
     public Integer publicInteger = 1;
 
+    static class DataBean{
+        int data = 0;
+        @Override
+        public boolean equals(Object obj) {
+            return obj != null && obj instanceof DataBean && ((DataBean) obj).data == this.data;
+        }
+    }
+
     public void publicMethodNoReturnThrowException() {
         throw new NullPointerException();
     }
 
     public String publicMethodReturnString() {
         return "publicMethodReturnString";
+    }
+
+    public int publicMethodReturnInt(){
+        return 999;
+    }
+
+    public DataBean publicMethodReturnObject(){
+        return new DataBean();
+    }
+
+    public List<Integer> publicMethodReturnList(){
+        return Collections.singletonList(1);
+    }
+
+    public int[] publicMethodReturnArray(){
+        return new int[]{1};
     }
 
     public String publicMethodReturnStringButThrowException() {
--- app/src/test/java/com/tufei/unittest/mockito/MockitoSampleTest.java
+++ app/src/test/java/com/tufei/unittest/mockito/MockitoSampleTest.java
@@ -6,9 +6,15 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 /**
@@ -25,21 +31,34 @@ public class MockitoSampleTest {
     @Before
     public void setup() {
         //当有多个要mock或者spy的类。不需要一个个调用mock(Class(T))。这更简洁。
+        //mockSample = mock(MockitoSample.class);
+        //spySample = spy(MockitoSample.class);
         MockitoAnnotations.initMocks(this);
     }
 
     /**
-     * mock无返回值的方法
+     * mock无返回值的方法:doNothing...when...
      */
     @Test
-    public void mockPublicMethodNoReturnThrowException() {
+    public void mockPublicMethodNoReturnThrowException_doNothing_when() {
         //无返回值没有对应的when...thenReturn...
         doNothing().when(mockSample).publicMethodNoReturnThrowException();
         mockSample.publicMethodNoReturnThrowException();
     }
 
     /**
-     * mock有返回值的方法
+     * mock有返回值的方法:when...thenReturn...
+     */
+    @Test
+    public void mockPublicMethodReturnString_when_thenReturn() {
+        String expected = "mockPublicMethodReturnString";
+        when(mockSample.publicMethodReturnString()).thenReturn(expected);
+        String actual = mockSample.publicMethodReturnString();
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * mock有返回值的方法:doReturn...thenReturn...
      */
     @Test
     public void mockPublicMethodReturnString() {
@@ -64,10 +83,82 @@ public class MockitoSampleTest {
      * 则默认的返回值是方法运行真实逻辑后返回的值。
      */
     @Test
-    public void notSpyPublicMethodReturnString_spyClass() {
+    public void notMockPublicMethodReturnString_spyClass() {
         String expected = "publicMethodReturnString";
         String actual = spySample.publicMethodReturnString();
         assertEquals(expected, actual);
+    }
+
+    /**
+     * 基本类型：数值类型默认返回0。boolean类型默认返回false。
+     */
+    @Test
+    public void notMockPublicMethodReturnInt_mockClass() {
+        int actual = mockSample.publicMethodReturnInt();
+        assertEquals(0, actual);
+    }
+
+    /**
+     * 返回方法执行后的实际返回值。
+     */
+    @Test
+    public void notMockPublicMethodReturnInt_spyClass() {
+        int actual = spySample.publicMethodReturnInt();
+        assertEquals(999, actual);
+    }
+
+    /**
+     * Object：默认返回null。
+     */
+    @Test
+    public void notMockPublicMethodReturnObject_mockClass() {
+        MockitoSample.DataBean actual = mockSample.publicMethodReturnObject();
+        assertEquals(null, actual);
+    }
+
+    /**
+     * 返回方法执行后的实际返回值。
+     */
+    @Test
+    public void notMockPublicMethodReturnObject_spyClass() {
+        MockitoSample.DataBean actual = spySample.publicMethodReturnObject();
+        assertEquals(new MockitoSample.DataBean(), actual);
+    }
+
+    /**
+     * 集合：默认返回空集合，并非null
+     */
+    @Test
+    public void notMockPublicMethodReturnList_mockClass() {
+        List<Integer> actual = mockSample.publicMethodReturnList();
+        assertEquals(new ArrayList<Integer>(), actual);
+    }
+
+    /**
+     * 返回方法执行后的实际返回值。
+     */
+    @Test
+    public void notMockPublicMethodReturnList_spyClass() {
+        List<Integer> actual = spySample.publicMethodReturnList();
+        assertEquals(Collections.singletonList(1), actual);
+    }
+
+    /**
+     * 数组：默认返回null
+     */
+    @Test
+    public void notMockPublicMethodReturnArray_mockClass() {
+        int[] actual = mockSample.publicMethodReturnArray();
+        assertArrayEquals(null, actual);
+    }
+
+    /**
+     * 返回方法执行后的实际返回值。
+     */
+    @Test
+    public void notMockPublicMethodReturnArray_spyClass() {
+        int[] actual = spySample.publicMethodReturnArray();
+        assertArrayEquals(new int[]{1}, actual);
     }
 
     /**
@@ -88,5 +179,18 @@ public class MockitoSampleTest {
     public void spyPublicMethodReturnStringButThrowException_when_thenReturn() {
         when(spySample.publicMethodReturnStringButThrowException()).thenReturn("test");
         mockSample.publicMethodReturnStringButThrowException();
+    }
+
+    /**
+     * 使用spy实例时，调用when...thenReturn...会走真实逻辑。spy(Object),其实就是通过
+     * Object的getClass方法，来获取到其Class实例。跟spy(Class<T>)一样没有什么实际区别。
+     * 看源码即可知道。
+     */
+    @Test(expected = NullPointerException.class)
+    public void spyObject_spyPublicMethodReturnStringButThrowException_when_thenReturn() {
+        MockitoSample mockitoSample = new MockitoSample();
+        MockitoSample spy = spy(mockitoSample);
+        when(spy.publicMethodReturnStringButThrowException()).thenReturn("test");
+        spy.publicMethodReturnStringButThrowException();
     }
 }
